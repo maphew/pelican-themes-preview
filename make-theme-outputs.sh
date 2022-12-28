769dfa63dcf8e68c3543f875d2444c57026b833c
matt wilkie <maphew@gmail.com>
2022-12-27 18:17:43 -0800

    initial check in

---
+++ make-theme-outputs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# silence warnings about missing images folder, which isn't in code repo 
+mkdir -p "pelican-blog/content/images"
+
+for i in pelican-themes/* ; do
+  if [ -d "$i" ]; then
+    theme=$(basename "$i")
+    echo "### $theme ###"
+    pelican \
+      pelican-blog/content \
+      --settings pelican-blog/pelicanconf.py \
+      --relative-urls \
+      --theme-path pelican-themes/$theme \
+      --output output/$theme \
+      --ignore-cache \
+      --delete-output-directory
+  fi
+done
