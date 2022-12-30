32c37b20672157055120702e6366e8f759da1f98
matt wilkie <maphew@gmail.com>
2022-12-29 21:53:49 -0700

    1st draft using new Samples content

--- make-theme-outputs.sh
+++ make-theme-outputs.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # silence warnings about missing images folder, which isn't in code repo 
-mkdir -p "pelican-blog/content/images"
+# mkdir -p "pelican-blog/content/images"
 
-for i in pelican-themes/* ; do
+for i in ../pelican-themes/a* ; do
   if [ -d "$i" ]; then
     theme=$(basename "$i")
     echo "### $theme ###"
     pelican \
-      pelican-blog/content \
-      --settings pelican-blog/pelicanconf.py \
+      samples/content \
+      --settings samples/pelican.conf.py \
       --relative-urls \
-      --theme-path pelican-themes/$theme \
+      --theme-path ../pelican-themes/$theme \
       --output output/$theme \
       --ignore-cache \
       --delete-output-directory \
