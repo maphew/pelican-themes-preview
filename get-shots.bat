10cda22a956e0e27e4c37f4245aa5e2b3293abe0
matt wilkie <maphew@gmail.com>
2022-12-31 01:47:08 -0700

    WIP: get screenshots of theme web pages
    Works on Windows, crashes on Codespaces. Not enough memory maybe?
    Annoying on Windows, a browser window pops open for each one.
    Need to research true headless.

---
+++ get-shots.bat
@@ -0,0 +1,1 @@
+for /d %%a in (output\*) do python webshot.py %%a
--- webshot.py
+++ webshot.py
@@ -9,13 +9,28 @@ Adapted from:
 - https://stackoverflow.com/questions/4091940/how-to-save-web-page-as-image-using-python
 - https://github.com/danielkaiser/python-chromedriver-binary
 '''
+import os
+import sys
 from selenium import webdriver
 import chromedriver_binary  # Adds chromedriver binary to path
 
+if len(sys.argv) < 2:
+    print("Error: No directory path provided")
+    sys.exit(1)
+
+dir_path = os.path.basename(sys.argv[1])
+prefix = "https://maphew.github.io/pelican-themes-preview"
+
+
+def get_shot(dir, prefix=prefix):
+    print(f"Fetching {prefix}/{dir}")
+    driver.get(f"{prefix}/{dir}")
+    # assert "Python" in driver.title
+    destination = f"screenshots/{dir}.png"
+    if driver.save_screenshot(destination):
+        print(f"Saved as {destination}")
+
+
 driver = webdriver.Chrome()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-destination = "screenshot.png"
-if driver.save_screenshot(destination):
-    print(f"Saved as {destination}")
+get_shot(dir_path)
 driver.quit()
